3881235886308095ca70633ee4ed8807e0e7a900
AndersonC <1megaword1@gmail.com>
2023-11-07 17:45:46 -0500

    new commit
    finish pokedex

--- index.js
+++ index.js
@@ -26,10 +26,9 @@ pokemon.then((res) => res.json())
 }
 
 function showPokemon(name, sprites, stats) {
-    let input = document.getElementById("pokeSearch");
-    input.placeholder = name;
+   
     let div = document.getElementById("pokename");
-    let table = `<table><tr><td>${name}</td></tr>`;
+    let table = `<table><tr><td class="text-center">${name}</td></tr>`;
   
     for (var i = 0; i < stats.length; i++) {
       table += `<tr><td>${stats[i].stat.name}</td><td>${stats[i].base_stat}</td></tr>`;
