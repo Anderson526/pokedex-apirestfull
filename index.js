03f806d4397f46e86382f0c3dbd01e0bc8d6c119
AndersonC <1megaword1@gmail.com>
2023-11-07 14:51:47 -0500

    new commit
    api integration pokenames c:

--- index.js
+++ index.js
@@ -8,5 +8,25 @@ pokemon.then((res) => res.json())
 .then((res2)=>{
 
     console.log(res2)
+    let {name,sprites,stats}= res2;
+    showPokemon(name,sprites,stats);
 })
 }
+
+function showPokemon(name, sprites, stats) {
+    let input = document.getElementById("pokeSearch");
+    input.placeholder = name;
+    let div = document.getElementById("pokename");
+  
+    let table = `<table><tr><td>${name}</td></tr>`;
+  
+    for (var i = 0; i < stats.length; i++) {
+      table += `<tr><td>${stats[i].stat.name}</td><td>${stats[i].base_stat}</td></tr>`;
+    
+    }
+  
+    table += `</table>`;
+  
+    div.innerHTML = table;
+  }
+  
